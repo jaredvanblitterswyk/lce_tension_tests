e2631dbad8f281eaaf3a54448bcdd42f473ff441
Jared Van Blitterswyk <59654680+jaredvanblitterswyk@users.noreply.github.com>
2021-03-17 18:50:45 -0400

    Add boxplot vs frame plot functionality

--- m_combine_dataframes.py
+++ m_combine_dataframes.py
@@ -31,7 +31,7 @@ dir_gom_results = os.path.join(dir_root,batch_ext,sample_ext,gom_ext)
 
 img_scale = 0.0106 # image scale (mm/pix)
 
-for i in range(0,31):
+for i in range(0,46):
     print('Adding frame: '+str(i))
     save_filename = 'results_df_frame_'+str(i)+'.pkl'
     frame_df = pd.read_pickle(os.path.join(dir_gom_results,save_filename))
@@ -61,7 +61,7 @@ plt.show()
 
 #%% Exploratory data analysis
 n_bins = 20
-frame_range = 25
+frame_range = 36
 num_img_x = 6
 num_img_y = int(round((frame_range-1)/num_img_x,0))
 
@@ -73,7 +73,6 @@ row, col = 0, 0 # current row and column number for plotting
 for i in range(1,frame_range):
     row = int(i/(num_img_x+0.1))
     col = i - row*(num_img_x) - 1
-    
     #print(str(row)+', '+str(col)) # check index logic
     
     # create dataframe for current frame
@@ -95,7 +94,6 @@ for i in range(1,frame_range):
     axs[row,col].set_xlim([0,1.05*single_frame_df['Exx'].max()])
     axs[row,col].tick_params(labelsize = 5)
     
-    
     # add line showning mean of field at each frame
     axs[row,col].axvline(agg_frame_df.loc[i,'Exx'], color='k', linestyle='dashed', linewidth=0.4, marker = '')
 
@@ -104,26 +102,69 @@ for i in range(1,frame_range):
 
 plt.show()
 
+# generate boxplot for each frame (Exx, Eyy, Exy)
+frame_labels = all_frames_df['frame'].unique().astype(int).astype(str)
+mpl.rcParams['lines.marker']=''
+
+
+data_exx = []
+data_eyy = []
+data_exy = []
+for i in range(0,frame_range):
+    data_exx.append(np.array(all_frames_df[all_frames_df['frame'] == i]['Exx']))
+    data_eyy.append(np.array(all_frames_df[all_frames_df['frame'] == i]['Eyy']))
+    data_exy.append(np.array(all_frames_df[all_frames_df['frame'] == i]['Exy']))
+
+#%%
+# mpl.rcParams['font.sans-serif'] = "Avenir LT Std"
+# # Then, "ALWAYS use sans-serif fonts"
+# mpl.rcParams['font.family'] = "sans-serif"
+
+boxprops = dict(linestyle='-', linewidth=0.5, color = '#4E598D')
+flierprops = dict(marker='o', markerfacecolor='#8D4E55', markersize=1,
+                  linestyle='-', linewidth = 0.1, markeredgecolor = '#4C161C',
+                  markeredgewidth = 0.0, alpha = 0.3)
+meanprops = dict(linestyle='-', linewidth=0.5, color = '#8D4E55')
+medianprops = dict(linestyle='-', linewidth=0.5, color = '#8D4E55')
+
+def plot_boxplot_vs_frame(data, ylabel):
+    # plot exx
+    f = plt.figure(figsize = (6,3))
+    ax = f.add_subplot(1,1,1)
+    ax.boxplot(data)#, boxprops = boxprops, flierprops = flierprops, 
+               #whiskerprops = boxprops, medianprops = medianprops, 
+               #capprops = boxprops)
+    ax.set_xticklabels(frame_labels)
+    ax.set_xlabel('Frame')
+    ax.set_ylabel(ylabel)
+    ax.grid(zorder=0)
+    plt.tight_layout()
+
+# plot strain box plots for each frame
+plot_boxplot_vs_frame(data_exx, ylabel = 'Exx')
+plot_boxplot_vs_frame(data_eyy, ylabel = 'Eyy')
+plot_boxplot_vs_frame(data_exy, ylabel = 'Exy')
+
+#%%
+
 # load in colormap
 cm_data = np.loadtxt('Z:/Python/mpl_styles/'+cmap_name+'.txt')
 custom_map = LinearSegmentedColormap.from_list('custom', cm_data)
 
 # ----- find strains in each 'mode' and plot stress-strain response -----
 # create dataframe for frame where bins clearly visible
-single_frame_df = all_frames_df[all_frames_df['frame'] == 17]
+single_frame_df = all_frames_df[all_frames_df['frame'] == 15]
 
 high_strain_df = single_frame_df[(single_frame_df['Exx'] > 1.6) &
                                  (single_frame_df['Exx'] < 1.8)]
 
 low_strain_df = single_frame_df[(single_frame_df['Exx'] > 0.7) &
-                                 (single_frame_df['Exx'] < 0.85)]
+                                 (single_frame_df['Exx'] < 0.8)]
 
 # plot high (mode 2) strain points
 p1 = high_strain_df.plot.scatter(x = 'x_pix', y = 'y_pix', s = 2, c = 'Exx')
 plt.xlim([0, 1200])
 plt.ylim([800,1150])
-#plt.clim(0.5, 1.85)
-#plt.set_cmap(custom_map)
 plt.tight_layout()
 plt.show()
 
@@ -131,7 +172,6 @@ plt.show()
 low_strain_df.plot.scatter(x = 'x_pix', y = 'y_pix', s = 2, c = 'Exx')
 plt.xlim([0, 1200])
 plt.ylim([800,1150])
-#plt.clim(0.5, 1.85)
 plt.tight_layout()
 plt.show()
 
@@ -150,18 +190,20 @@ agg_low_strain_sample = all_frames_df[
     (all_frames_df['y_pix'].isin(low_strain_sample['y_pix']))]
 
 # generate scatter plots
-f = plt.figure()
+f = plt.figure(figsize = (3,3))
 ax = f.add_subplot(1,1,1)
-ax.scatter(agg_high_strain_sample['Exx'], agg_high_strain_sample['stress_mpa'],
+ax.scatter(agg_high_strain_sample['frame'], agg_high_strain_sample['Exx'],
            s = 3, c = '#4E598D', edgecolors = '#262C47', 
            linewidths = 0.5, label = '1.6 < Exx < 1.8')
-ax.scatter(agg_low_strain_sample['Exx'], agg_low_strain_sample['stress_mpa'],
+ax.scatter(agg_low_strain_sample['frame'], agg_low_strain_sample['Exx'],
            s = 3, c = '#8D4E55', edgecolors = '#4C161C',
            linewidths = 0.5, label = '0.7 < Exx < 0.85')
 ax.grid(True, alpha = 0.5)
-ax.set_xlabel('Exx', fontsize = 6)
-ax.set_ylabel('Avg x-section stress (MPa)', fontsize = 6)
-plt.legend(loc='upper left', fontsize = 6);
+ax.set_xlim([0, len(files_gom)+2])
+ax.set_xlabel('Frame', fontsize = 6)
+ax.set_ylabel('Exx', fontsize = 6)
+plt.legend(loc='upper left', fontsize = 6)
+plt.tight_layout()
 plt.show()
 '''
 agg_high_strain_sample.plot.scatter(x = 'Exx', y = 'stress_mpa', s = 8, c = '#4E598D')
