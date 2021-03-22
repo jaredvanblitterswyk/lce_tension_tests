ce4208f4ce0bd677fe0a450cd75b744198d1eda1
Jared Van Blitterswyk <jared.vanblitterswyk@nist.gov>
2021-03-22 14:26:08 -0400

    Fix bug in strain vs frame plot for single frame processing

--- m_eda_multiple_frames.py
+++ m_eda_multiple_frames.py
@@ -214,14 +214,18 @@ plot_boxplot_vs_frame(data_exy, ylabel = 'Exy')
 # ----------------------------------------------------------------------------
 # ----- filter data into strain ranges based on selected frame -----
 # ----------------------------------------------------------------------------
-# create dataframe for frame where bins clearly visible
+# create dataframe for frame used to define coordinates in strain bands 
 mask_frame = 15
-single_frame_df = all_frames_df[all_frames_df['frame'] == mask_frame]
-
-max_strain_band= round(single_frame_df['Exx'].max(),1)
 num_strain_bands = 9
+strain_component = 'Exy'
+frame_range = 20
 
-strain_ranges = np.linspace(0.2, max_strain_band, num_strain_bands)
+# calculate strain range bounds
+max_strain_band = round(single_frame_df[strain_component].max(),1)
+min_strain_band = round(single_frame_df[strain_component].min(),1)
+strain_ranges = np.linspace(min_strain_band, max_strain_band, num_strain_bands)
+#Exy
+strain_ranges = np.linspace(min_strain_band, max_strain_band, num_strain_bands)
 
 # colour and edge colour arrays (hard coded to 10 or less strain bands)
 ec = ['#003057', '#00313C', '#9E2A2F', '#623412','#284723',
@@ -229,56 +233,252 @@ ec = ['#003057', '#00313C', '#9E2A2F', '#623412','#284723',
 c = ['#BDD6E6', '#8DB9CA', '#F4C3CC', '#D9B48F', '#9ABEAA',
      '#C9B1D0', '#F1C6A7', '#C8C9C7', '#88DBDF', '#C1B2B6']
 
-# plot on one figure
-f = plt.figure(figsize = (3,3))
-ax = f.add_subplot(1,1,1)
-for i in range(4,num_strain_bands+1):
-    if i == num_strain_bands:
-        strain_band_df = single_frame_df[
-            single_frame_df['Exx'] >= strain_ranges[i-1]]   
-    else:
-        strain_band_df = single_frame_df[(
-            single_frame_df['Exx'] >= strain_ranges[i-1]
-            ) & (
-            single_frame_df['Exx'] < strain_ranges[i]
-            )]
-            
-    # find all points within that strain range
-    agg_strain_band_df = all_frames_df[
-    all_frames_df.index.isin(strain_band_df.index)]
-    
-    if agg_strain_band_df.shape[0] > 2000:
-        strain_band_sample = agg_strain_band_df.sample(n = 2000, 
-                                                       random_state = 1)
-    else:
-        strain_band_sample = agg_strain_band_df.copy()
-    
-    # add to plot
-    ax.scatter(strain_band_sample['Exx'], 
-               strain_band_sample['stress_mpa'],
-               s = 2, 
-               c = c[i-1],
-               edgecolors = ec[i-1], 
-               alpha = 0.4,
-               linewidths = 0.5, 
-               label = 'strain_band: '+str(i))
-    ax.set_xlim([0,round(all_frames_df['Exx'].max(),1)+0.5])
-    ax.set_ylim([0,round(all_frames_df['stress_mpa'].max(),1)+0.05])
-    ax.grid(True, alpha = 0.5,zorder = 0)
-    ax.set_xlabel('Exx')
-    ax.set_ylabel('Stress (Mpa)')
-plt.legend(loc='upper left', fontsize = 4)
-legend = ax.legend(fontsize = 4)
-legend.get_frame().set_linewidth(0.5)
-'''
-# add line showning mean of field at each frame
-ax.axvline(mask_frame, linestyle='dashed', linewidth=0.4, marker = '')
-_, max_ylim = plt.ylim()
-ax.text(mask_frame*1.1, max_ylim*0.8, 
-        'Mask: {:.0f}'.format(mask_frame), 
-        fontsize = 4)
-'''
+num_img_x = 3
+num_img_y = int((num_strain_bands)/num_img_x)
 
+fig, axs = plt.subplots(num_img_y, num_img_x, figsize=(5,5),
+                        sharey=True, tight_layout=True)
+
+# generate plot (either load in all at once, or individually - memory issues)
+if load_multiple_frames:
+    single_frame_df = all_frames_df[all_frames_df['frame'] == mask_frame]
+    
+    for i in range(1,num_strain_bands+1):
+        row = int(i/(num_img_x+0.1))
+        col = i - row*(num_img_x) - 1
+        #print(str(row)+', '+str(col)) # check index logic
+    
+        if i == num_strain_bands:
+            strain_band_df = single_frame_df[
+                single_frame_df[strain_component] >= strain_ranges[i-1]]   
+        else:
+            strain_band_df = single_frame_df[(
+                single_frame_df[strain_component] >= strain_ranges[i-1]
+                ) & (
+                single_frame_df[strain_component] < strain_ranges[i]
+                )]
+                
+        # find all points within that strain range
+        agg_strain_band_df = all_frames_df[
+        all_frames_df.index.isin(strain_band_df.index)]
+        
+        if agg_strain_band_df.shape[0] > 2000:
+            strain_band_sample = agg_strain_band_df.sample(n = 2000, 
+                                                           random_state = 1)
+        else:
+            strain_band_sample = agg_strain_band_df.copy()
+        
+        # add to plot
+        axs[row,col].scatter(strain_band_sample['frame'], 
+                             strain_band_sample[strain_component],
+                             s = 2, 
+                             c = c[i-1], 
+                             edgecolors = ec[i-1], 
+                             alpha = 0.4,
+                             linewidths = 0.5)
+        axs[row,col].set_xlim([0,len(all_frames_df['frame'].unique())+2])
+        axs[row,col].set_ylim([0,round(
+            all_frames_df[strain_component].max(),1
+            )
+            +0.5
+            ])
+        axs[row,col].set_xlabel('Frame')
+        axs[row,col].set_ylabel(strain_component)
+        axs[row,col].grid(True, alpha = 0.5,zorder = 0)
+        if i == num_strain_bands:
+            axs[row,col].set_title(
+                'strain_band: '+'>'+str(round(strain_ranges[i-1],1)),
+                fontsize = 5
+                )
+        else:       
+            axs[row,col].set_title(
+                'strain_band: '+str(round(strain_ranges[i-1],1))+
+                ':'+ str(round(strain_ranges[i],1)), 
+                fontsize = 5
+                )
+    
+        # add line showning mean of field at each frame
+        axs[row,col].axvline(mask_frame, linestyle='dashed', 
+                             linewidth=0.4, marker = '')
+    
+        _, max_ylim = plt.ylim()
+        axs[row,col].text(mask_frame*1.1, max_ylim*0.8,
+                          'Mask frame: {:.0f}'.format(mask_frame),
+                          fontsize = 4)
+    
+else:
+    save_filename = 'results_df_frame_'+str(mask_frame)+'.pkl'
+    single_frame_df = pd.read_pickle(
+        os.path.join(dir_gom_results,save_filename)
+        )
+
+    # create a list of indices corresponding to each strain band
+    strain_band_indices = []
+    for i in range(1,num_strain_bands+1):
+        if i == num_strain_bands:
+            strain_band_df = single_frame_df[
+                single_frame_df[strain_component] >= strain_ranges[i-1]]   
+        else:
+            strain_band_df = single_frame_df[(
+                single_frame_df[strain_component] >= strain_ranges[i-1]
+                ) & (
+                single_frame_df[strain_component] < strain_ranges[i]
+                )]
+                    
+        strain_band_indices.append(strain_band_df.index)
+
+    # ----- plot strain bands vs time -----
+    for j in range(0,frame_range):
+        print('Plotting frame: '+str(j))
+        save_filename = 'results_df_frame_'+str(j)+'.pkl'
+        curr_frame_df = pd.read_pickle(
+            os.path.join(dir_gom_results,save_filename)
+            )
+        
+        # add time stamp to frame to allow for sorting later
+        curr_frame_df['frame'] = j*np.ones((curr_frame_df.shape[0],))      
+        row, col = 0, 0 # current row and column number for plotting
+        for i in range(1,num_strain_bands+1):
+            row = int(i/(num_img_x+0.1))
+            col = i - row*(num_img_x) - 1
+            #print(str(row)+', '+str(col)) # check index logic
+                    
+            # find all points within that strain range
+            strain_band = curr_frame_df[
+                curr_frame_df.index.isin(strain_band_indices[i-1])
+                ]
+                                      
+            
+            if strain_band.shape[0] > 2000:
+                strain_band_sample = strain_band.sample(
+                    n = 2000, random_state = 1
+                    )
+            else:
+                strain_band_sample = strain_band.copy()
+            
+            # add to plot
+            axs[row,col].scatter(strain_band_sample['frame'], 
+                                 strain_band_sample[strain_component],
+                                 s = 2, 
+                                 c = c[i-1], 
+                                 edgecolors = ec[i-1], 
+                                 alpha = 0.4,
+                                 linewidths = 0.5)
+            axs[row,col].set_xlim([0,frame_range+2])
+            axs[row,col].set_ylim([min_strain_band-0.1,max_strain_band+0.1])
+            axs[row,col].set_xlabel('Frame')
+            axs[row,col].set_ylabel(strain_component)
+            axs[row,col].grid(True, alpha = 0.5,zorder = 0)
+            if i == num_strain_bands:
+                axs[row,col].set_title(
+                    'strain_band: '+'>'+str(round(strain_ranges[i-1],1)),
+                    fontsize = 5
+                    )
+            else:       
+                axs[row,col].set_title(
+                    'strain_band: '+str(round(strain_ranges[i-1],1))+
+                    ':'+ str(round(strain_ranges[i],1)), 
+                    fontsize = 5
+                    )
+            
+            # add line showning mean of field at each frame
+            axs[row,col].axvline(mask_frame, linestyle='dashed', 
+                                 linewidth=0.4, marker = '')
+        
+            _, max_ylim = plt.ylim()
+            axs[row,col].text(mask_frame*1.1, max_ylim*0.8,
+                              'Mask frame: {:.0f}'.format(mask_frame),
+                              fontsize = 4)    
+
+#%% find points with high shear strain and high axial strain
+# hard code strain rnages based on results from plot of strain band vs. frame
+mask_frame = 10
+exx_max = 0.7
+exy_min = -0.15
+exy_max = 0.15
+
+if ~load_multiple_frames:
+    save_filename = 'results_df_frame_'+str(mask_frame)+'.pkl'
+    single_frame_df = pd.read_pickle(
+        os.path.join(dir_gom_results,save_filename)
+        )
+
+    # filter based on strain values 
+    strain_band_exy_df = single_frame_df[(
+        single_frame_df['Exy'] >= exy_max
+        ) | (
+        single_frame_df['Exy'] < exy_min
+        )
+    ]   
+
+    strain_band_exx_df = single_frame_df[
+        single_frame_df['Exx'] >= exx_max
+        ]
+    
+    excl_strain_band_df = single_frame_df[(
+        single_frame_df.index.isin(strain_band_exy_df.index) == False)
+        & (
+        single_frame_df.index.isin(strain_band_exx_df.index) == False)
+        ]
+    
+    # plot on one figure
+    f = plt.figure(figsize = (2,3))
+    ax = f.add_subplot(1,1,1)
+    ax.scatter(strain_band_exx_df[['x_pix']], 
+                   strain_band_exx_df[['y_pix']], 
+                   s = 1, 
+                   c = '#003057', 
+                   edgecolors = '#003057', 
+                   alpha = 0.4,
+                   linewidths = 0, zorder = 0)
+    ax.scatter(strain_band_exy_df[['x_pix']], 
+               strain_band_exy_df[['y_pix']], 
+               s = 1, 
+               c = '#9E2A2F', 
+               edgecolors = '#9E2A2F', 
+               alpha = 0.4,
+               linewidths = 0, zorder = 1)
+    ax.scatter(excl_strain_band_df[['x_pix']], 
+                   excl_strain_band_df[['y_pix']], 
+                   s = 1, 
+                   c = '#D0D3D4', 
+                   edgecolors = '#D0D3D4', 
+                   alpha = 0.4,
+                   linewidths = 0, zorder = -1)
+    ax.grid(True, alpha = 0.5)
+    ax.set_xlabel('x (pix)')
+    ax.set_ylabel('y (pix)')
+    ax.set_title('Exx > '+str(exx_max)+', '+str(exy_min)+'< Exy <= '+str(exy_max))
+    
+    plt.tight_layout()
+    
+
+    '''    
+    # plot on one figure
+    f = plt.figure(figsize = (2,3))
+    ax = f.add_subplot(1,1,1)
+    ax.scatter(strain_band_exy_df[['x_pix']], 
+               strain_band_exy_df[['y_pix']], 
+               s = 1, 
+               c = '#9E2A2F', 
+               edgecolors = '#9E2A2F', 
+               alpha = 0.4,
+               linewidths = 0, zorder = 0)
+    
+    ax.scatter(excl_strain_band_df[['x_pix']], 
+                   excl_strain_band_df[['y_pix']], 
+                   s = 1, 
+                   c = '#D0D3D4', 
+                   edgecolors = '#D0D3D4', 
+                   alpha = 0.4,
+                   linewidths = 0, zorder = -1)
+    ax.grid(True, alpha = 0.5)
+    ax.set_xlabel('x (pix)')
+    ax.set_ylabel('y (pix)')
+    ax.set_title('Exy < -0.2 & Exy > 0.2')
+    
+    plt.tight_layout()
+    '''
 #%%    
 # generate sub plots for each strain band
 num_img_x = 3
@@ -341,7 +541,49 @@ for i in range(1,num_strain_bands+1):
     axs[row,col].text(mask_frame*1.1, max_ylim*0.8,
                       'Mask frame: {:.0f}'.format(mask_frame),
                       fontsize = 4)
-    
+#%% - plot stress strain for each strain band on one plot
+
+# plot on one figure
+    f = plt.figure(figsize = (3,3))
+    ax = f.add_subplot(1,1,1)
+    for i in range(4,num_strain_bands+1):
+        if i == num_strain_bands:
+            strain_band_df = single_frame_df[
+                single_frame_df['Exx'] >= strain_ranges[i-1]]   
+        else:
+            strain_band_df = single_frame_df[(
+                single_frame_df['Exx'] >= strain_ranges[i-1]
+                ) & (
+                single_frame_df['Exx'] < strain_ranges[i]
+                )]
+                
+        # find all points within that strain range
+        agg_strain_band_df = all_frames_df[
+        all_frames_df.index.isin(strain_band_df.index)]
+        
+        if agg_strain_band_df.shape[0] > 2000:
+            strain_band_sample = agg_strain_band_df.sample(n = 2000, 
+                                                           random_state = 1)
+        else:
+            strain_band_sample = agg_strain_band_df.copy()
+        
+        # add to plot
+        ax.scatter(strain_band_sample['Exx'], 
+                   strain_band_sample['stress_mpa'],
+                   s = 2, 
+                   c = c[i-1],
+                   edgecolors = ec[i-1], 
+                   alpha = 0.4,
+                   linewidths = 0.5, 
+                   label = 'strain_band: '+str(i))
+        ax.set_xlim([0,round(all_frames_df['Exx'].max(),1)+0.5])
+        ax.set_ylim([0,round(all_frames_df['stress_mpa'].max(),1)+0.05])
+        ax.grid(True, alpha = 0.5,zorder = 0)
+        ax.set_xlabel('Exx')
+        ax.set_ylabel('Stress (Mpa)')
+    plt.legend(loc='upper left', fontsize = 4)
+    legend = ax.legend(fontsize = 4)
+    legend.get_frame().set_linewidth(0.5)
 #%% Take last group and overlay spatial mask - run after cell above
 strain_band = 9
 
