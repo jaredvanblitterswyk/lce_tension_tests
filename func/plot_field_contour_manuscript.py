3bfd5e396af41a7777876b9f30ba68667ab36e49
Jared Van Blitterswyk <jared.vanblitterswyk@nist.gov>
2021-10-25 12:32:31 -0400

    Add function with additional customization for manuscript figures

--- func/plot_field_contour_manuscript.py
+++ func/plot_field_contour_manuscript.py
@@ -6,9 +6,10 @@ Created on Wed Jul 14 14:23:32 2021
 """
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
+import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def plot_field_contour_save(xx, yy, zz, plot_params, title):
+def plot_field_contour_manuscript(xx, yy, zz, plot_params, title = None):
     '''Generate contour plot of field response for one variable at a specified 
     frame number
     
@@ -17,17 +18,23 @@ def plot_field_contour_save(xx, yy, zz, plot_params, title):
         yy (array/list): y coordinates
         zz (array/list): magnitude of variable at each coordinate
         plot_params (dict): dictionary of parameters to customize plot
-        i (int): frame number
+        title (string): figure title (optional)
             
     Returns:
         Figure
     '''
+    v1 = np.linspace(plot_params['vmin'], plot_params['vmax'], 7, endpoint=True)   
+    
     # toggle interactive mode
     if not plot_params['show_fig']:
         plt.ioff()
     
     # plot map    
     f = plt.figure(figsize = plot_params['figsize'])
+    f.subplots_adjust(left = plot_params['bbox'][0],
+                      right = plot_params['bbox'][1],
+                      top = plot_params['bbox'][2],
+                      bottom = plot_params['bbox'][3])
     ax = f.add_subplot(1,1,1)
     cf = ax.scatter(xx, yy, c = zz, 
                     s = plot_params['m_size'],  
@@ -39,11 +46,14 @@ def plot_field_contour_save(xx, yy, zz, plot_params, title):
     # set axes features/limits
     ax.set_xlim(plot_params['xlims'])
     ax.set_ylim(plot_params['ylims'])
-    ax.grid(True, alpha = plot_params['grid_alpha'], zorder = -1)
-    ax.set_title(title)
+    ax.grid(True, alpha = plot_params['grid_alpha'], 
+            linewidth = plot_params['grid_lw'], zorder = -1)
+    if title:
+        ax.set_title(title)
     if plot_params['cbar']:
-        cbar = f.colorbar(cf)
+        cbar = f.colorbar(cf, ticks=v1)
         cbar.ax.set_ylabel(plot_params['var_name'])
+        cbar.ax.set_yticklabels(["{:4.2f}".format(i) for i in v1])
 
     # show grid but hide labels
     if plot_params['hide_labels']:
@@ -52,8 +62,12 @@ def plot_field_contour_save(xx, yy, zz, plot_params, title):
         ax.yaxis.set_ticklabels([])
         ax.yaxis.set_ticks_position('none')    
     else:
-        ax.set_xlabel(plot_params['xlabel'])
-        ax.set_ylabel(plot_params['ylabel'])
+        ax.set_xlabel(plot_params['xlabel'], fontsize = plot_params['fontsize'])
+        ax.set_ylabel(plot_params['ylabel'], fontsize = plot_params['fontsize'])
+        ax.tick_params(axis='both', which='major', labelsize = plot_params['fontsize'])
+        
+    for axis in ['left', 'right', 'top', 'bottom']:
+        ax.spines[axis].set_linewidth(plot_params['spine_lw'])
     
     if plot_params['tight_layout']:
         plt.tight_layout()  
